58905c38b15911aa3fa31708a2100ab91e9cd3ca
GlebDot <bessudnov.gleb@gmail.com>
2021-12-23 21:55:54 +0300

    backend fixes

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, url_for
 import pymongo
 import json
+from bson.json_util import dumps
 
 ### Setup DB
 client = pymongo.MongoClient()
 db = client["sky_map"]
-hello_db = db["hello_db"]
+hello_db = db["celestial_bodies"]
 
 ### Create app
 app = Flask(__name__)
@@ -18,8 +19,8 @@ def main():
     return "<p>Hello, sky objects!</p>"
 
 ### list elements
-@app.route("/sky_map/",  methods=["GET"])
-def list_sky_map():
+@app.route("/sky_map_list", methods=["GET"])
+def sky_map_list():
     
     ## use 'page' parameter for pagination
     page = int(request.args.get("page", 1))
@@ -27,32 +28,33 @@ def list_sky_map():
 
     ## find all docs for specific page
     docs = hello_db.find().sort("name").skip(per_page * (page - 1)).limit(per_page)
+    print(docs)
     docs_count = hello_db.count_documents({})
     links = {
-        "self": {"href": url_for(".sky_map", page=page, _external=True)},
+        "self": {"href": url_for(".sky_map_list", page=page, _external=True)},
     }
 
     ## link for the prev page
     if page > 1:
         links["prev"] = {
-            "href": url_for(".sky_map", page=page - 1, _external=True)
+            "href": url_for(".sky_map_list", page=page - 1, _external=True)
         }
 
     ## link for the next page
     if page - 1 < docs_count // per_page:
         links["next"] = {
-            "href": url_for(".sky_map", page=page + 1, _external=True)
+            "href": url_for(".sky_map_list", page=page + 1, _external=True)
         }
     
     ### here we will return list of sky objects and links for buttons
     return {
-        "sky_objects": [doc.to_json() for doc in docs],
+        "sky_objects": [dumps(doc) for doc in docs],
         "links": links,
     }
 
 ### inserting new object
-@app.route("/sky_map/", methods=["POST"])
-def new_cocktail():
+@app.route("/sky_map_insert", methods=["POST"])
+def sky_map_insert():
     ### here we take json from request. make sure that in the form we are sending json with right structure
     sky_object = request.get_json()
     hello_db.insert_one(sky_object)
