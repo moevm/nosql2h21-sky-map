9a96cbf603edb88728e52a1f8258d5c59431d9d5
Pchooo <42907700+Pchooo@users.noreply.github.com>
2021-12-25 00:07:55 +0300

    frontend fixes

--- skymap/routes/index.js
+++ skymap/routes/index.js
@@ -6,6 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Sky map' });
 });
 router.get('/2', function(req, res, next) {
-  res.render('search_page', { title: 'Sky map' });
+  res.render('search_page', { Name: 'Name', Type: 'Type', Num: '1' });
+});
+router.get('/3', function(req, res, next) {
+  res.render('card', { Name: 'Sky map 1' , info: 'some info'});
 });
 module.exports = router;
