edba8b860753f45c8aa0fcdbac1ddf65d9b4a4b0
GlebDot <bessudnov.gleb@gmail.com>
2021-12-25 14:22:47 +0300

    backend update

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,12 @@
-from flask import Flask, request, url_for
+from flask import Flask, request, url_for, render_template
 import pymongo
 import json
-from bson.json_util import dumps
+from bson import json_util
 
 ### Setup DB
 client = pymongo.MongoClient()
 db = client["sky_map"]
-hello_db = db["celestial_bodies"]
+collection = db["celestial_bodies"]
 
 ### Create app
 app = Flask(__name__)
@@ -16,7 +16,13 @@ app = Flask(__name__)
 @app.route("/home")
 def main():
     ### paste here some fancy page
-    return "<p>Hello, sky objects!</p>"
+    return render_template("index.html", title="Sky Map")
+
+@app.route("/sky_object")
+def sky_object():
+    pgc = request.args.get("object_pgc", 1)
+    doc = collection.find_one({"pgc": pgc}, {"_id": 0})
+    return render_template("card.html", obj=doc)
 
 ### list elements
 @app.route("/sky_map_list", methods=["GET"])
@@ -27,9 +33,9 @@ def sky_map_list():
     per_page = 20
 
     ## find all docs for specific page
-    docs = hello_db.find().sort("name").skip(per_page * (page - 1)).limit(per_page)
+    docs = collection.find().sort("name").skip(per_page * (page - 1)).limit(per_page)
     print(docs)
-    docs_count = hello_db.count_documents({})
+    docs_count = collection.count_documents({})
     links = {
         "self": {"href": url_for(".sky_map_list", page=page, _external=True)},
     }
@@ -45,17 +51,23 @@ def sky_map_list():
         links["next"] = {
             "href": url_for(".sky_map_list", page=page + 1, _external=True)
         }
+
+    # for doc in docs:
+    #     doc["link"] = {
+    #         "href": url_for(".sky_object", object_pgc=doc["pgc"], _external=True)
+    #     }
     
     ### here we will return list of sky objects and links for buttons
-    return {
-        "sky_objects": [dumps(doc) for doc in docs],
+    info = {
+        "sky_objects": docs,
         "links": links,
     }
+    return render_template("search.html", data=info)
 
 ### inserting new object
 @app.route("/sky_map_insert", methods=["POST"])
 def sky_map_insert():
     ### here we take json from request. make sure that in the form we are sending json with right structure
     sky_object = request.get_json()
-    hello_db.insert_one(sky_object)
+    collection.insert_one(sky_object)
     return json.dump(sky_object)
